cc9f88a577d0ed0b00eb1af5a37c4e294ce664d4
Fermin Acuña <fermin.unicen@gmail.com>
2021-12-06 08:20:47 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.listen( 4500 , () => {
+    console.log('servidor corriendo en puerto 4500')
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        "grafica": {
+            "facebook": 2700,
+            "youtube": 2000,
+            "whatsapp": 1400,
+            "facebook-messenger": 1350,
+            "instagram": 1123
+        }
+    })
+})
